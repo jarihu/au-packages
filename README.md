0200726659b2aa8f1c696e5add83d0f3a356fc55
Jari <jari@genvr.com>
2025-04-13 11:00:22 +0300

    Support Phorest Desktop only

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
+Older Phorest versions are no longer supported. Only Phorest Desktop releases are supported.
+
 [![](https://ci.appveyor.com/api/projects/status/github/jarihu/chocolatey-packages?svg=true)](https://ci.appveyor.com/project/jarihu/chocolatey-packages)
 [Update status](https://gist.github.com/jarihu/YOUR_GIST_ID_HERE)
 [![](http://transparent-favicon.info/favicon.ico)](#)
 [chocolatey/jarihu](https://chocolatey.org/profiles/jarihu)
-
--- phorest/update.ps1
+++ phorest/update.ps1
@@ -1,7 +1,8 @@
 import-module au
 
-$releases = 'https://www.phorest.com/us/downloads/'
-$regex = '\.exe'
+$releases = 'https://update-distribution.phorest.com/electron/downloads/win32/x64/'
+$versionRegex = 'Phorest%20Desktop-([0-9]+\.[0-9]+\.[0-9]+)%20Setup\.exe$'
+
 function global:au_SearchReplace {
     @{
         "tools\chocolateyInstall.ps1" = @{
@@ -15,8 +16,8 @@ function global:au_GetLatest {
      $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing #1
      $regex   = '.exe$'
      $url     = $download_page.links | ? href -match $regex | select -First 1 -expand href #2
-	 $url -match '_([0-9]+_[0-9]+_[0-9]+)\.exe'
-	 $version = $Matches[1] -replace '_','.'
+	 $url -match $versionRegex
+	 $version = $Matches[1]
      return @{ Version = $version; URL32 = $url }
 }
 
